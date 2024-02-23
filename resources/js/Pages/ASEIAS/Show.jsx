3a0da13ccf49a2c3a8673a95e4de98d8ac873fdd
Luis Molina <alfonsomduque@gmail.com>
2024-02-23 15:44:05 -0600

    Public Navbar Component, y uso en ASEIAS/SHOW

--- resources/js/Pages/ASEIAS/Show.jsx
+++ resources/js/Pages/ASEIAS/Show.jsx
@@ -1,6 +1,42 @@
+import PublicLayout from "@/Layouts/PublicLayout/PublicNavbar";
 import Navbar from "@/Layouts/PublicLayout/PublicNavbar";
 import React from "react";
+import { Head } from "@inertiajs/react";
 
-function Show() {}
+function Show() {
+    /*  return <GuestLayout></GuestLayout>; */
+    return (
+        <PublicLayout
+            logoUrl="/path-to-your-logo.png"
+            logoText="Your App Name"
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Public
+                </h2>
+            }
+            links={[
+                { href: "/", text: "Home" },
+                { href: "/about", text: "About" },
+                { href: "/contact", text: "Contact" },
+            ]}
+            dropdownLinks={[
+                { href: "/profile/edit", text: "Profile" },
+                { href: "/logout", text: "Log Out" },
+            ]}
+        >
+            {/* Contenido de la página aquí */}
+            <Head title="ASEIAS" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            Bienvenido a ASEIAS
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </PublicLayout>
+    );
+}
 
 export default Show;
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AseiasPublic;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -35,4 +36,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+//!Public
+Route::get('/public', [AseiasPublic::class, 'show'])->name('aseias.show');
+
+require __DIR__ . '/auth.php';
